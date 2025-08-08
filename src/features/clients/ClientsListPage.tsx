d662bba326d17f2a35b3be6845ef20687938723f
Chrisrossi92 <chrisrossi92@yahoo.com>
2025-08-08 05:58:42 -0700

    Create ClientsListPage.tsx

---
+++ src/features/clients/ClientsListPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * Placeholder clients list page. This will show the list of clients with
+ * filters and allow navigation to individual client profiles.
+ */
+const ClientsListPage: React.FC = () => {
+  return <div className="text-lg font-semibold">Clients list placeholder</div>;
+};
+
+export default ClientsListPage;
